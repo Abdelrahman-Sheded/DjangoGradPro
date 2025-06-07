aa2f3388521ca66f4af99216a8e0b67082da8a2d
Abdelrahman-Sheded <62433988+Abdelrahman-salah12@users.noreply.github.com>
2025-06-07 18:02:13 +0300

    deployed on railway

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,14 +80,10 @@ WSGI_APPLICATION = "config.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get('POSTGRES_DB', 'hiring'),
-        "USER": os.environ.get('POSTGRES_USER', 'postgres'),
-        "PASSWORD": os.environ.get('POSTGRES_PASSWORD', 'admin'),
-        "HOST": os.environ.get('POSTGRES_HOST', 'localhost'),
-        "PORT": os.environ.get('POSTGRES_PORT', '5432'),
-    }
+    "default": dj_database_url.config(
+        default=os.environ.get('DATABASE_URL', 'postgresql://postgres:jRaETciQWBgyBRWTYezlyVYdQjhlJcDG@centerbeam.proxy.rlwy.net:30764/railway'),
+        conn_max_age=600
+    )
 }
 
 
